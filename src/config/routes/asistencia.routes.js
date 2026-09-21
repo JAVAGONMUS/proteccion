0e1fefe3508a2ae7a4f7e4ac1fcfa755bd4fe530
JAVAGONMUS <93622691+JAVAGONMUS@users.noreply.github.com>
2026-09-20 19:16:41 -0600

    Update database path in asistencia.routes.js

--- src/config/routes/asistencia.routes.js
+++ src/config/routes/asistencia.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
-const pool = require('../config/db');
+const pool = require('../db');
 const router = express.Router();
 
 // -------------------------------------------------------------
